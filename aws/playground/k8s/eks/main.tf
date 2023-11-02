64924994c157beec8f40877e528c0cdb7bc9d411
fomenky <fomenky@gmail.com>
2023-11-02 17:34:35 +0000

    deployed eks

--- aws/playground/k8s/eks/main.tf
+++ aws/playground/k8s/eks/main.tf
@@ -16,13 +16,6 @@ resource "aws_eks_cluster" "eks" {
   vpc_config {
     subnet_ids = var.subnets
   }
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.eks-AmazonEKSVPCResourceController,
-  ]
   
   tags = merge(
         module.eks_tags.tags,
@@ -43,21 +36,4 @@ data "aws_iam_policy_document" "assume_role" {
 
     actions = ["sts:AssumeRole"]
   }
-}
-
-# resource "aws_iam_role" "eks_iam_role" {
-#   name               = "eks-cluster-iam-role"
-#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-# }
-
-resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = data.aws_iam_role.eks_iam_role.name
-}
-
-# Optionally, enable Security Groups for Pods
-# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
-resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = data.aws_iam_role.eks_iam_role.name
 }
